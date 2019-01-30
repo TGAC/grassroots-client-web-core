d419137170107f90985c2baf640009f395f8838f
billyfish <tyrrell.simon@gmail.com>
2019-01-30 13:27:07 +0000

    fixed request for specific field trial

--- scripts/fieldtrial_map.js
+++ scripts/fieldtrial_map.js
@@ -481,15 +481,14 @@ function CreatePlotsRequestForExperimentalArea (exp_area_id) {
 		    "start_service": true,
 		    "parameter_set": {
 			    "parameters": [{
-				    "param": "Experimental Area to search for",
-				    "current_value": "5bcdc979618dc26d682e4a52",
-				    "grassroots_type": "xsd:string",
-				    "group": "Experimental Area"
+				    "param": "Study to search for",
+				    "current_value": "5bcdc979618dc26d682e4a52"
 			    }, {
-				    "param": "Get all Plots for Experimental Area",
-				    "current_value": true,
-				    "grassroots_type": "xsd:boolean",
-				    "group": "Experimental Area"
+				    "param": "Get all Plots for Study",
+				    "current_value": true
+			    }, {
+				    "param": "Search Studies",
+				    "current_value": true
 			    }]
 		    }
 	    }]
