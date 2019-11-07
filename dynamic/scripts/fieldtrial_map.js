2419f66af1850e810ed0f3b313a63a2beb54fe4a
Xingdong Bian <bianxingdong@gmail.com>
2019-11-07 12:23:16 +0000

    fix field trial request json

--- dynamic/scripts/fieldtrial_map.js
+++ dynamic/scripts/fieldtrial_map.js
@@ -607,31 +607,24 @@ function CreatePlotsRequestForFieldTrial(fieldtrial_id) {
             "so:name": "Search Field Trials",
             "parameter_set": {
                 "level": "advanced",
-                "parameters": [{
-                    "param": "FT Id",
-                    "current_value": fieldtrial_id
-                }, {"param": "FT Name", "current_value": ""}, {
-                    "param": "FT Team",
-                    "current_value": ""
-                }, {"param": "Study to search for", "current_value": ""}, {
-                    "param": "Locations",
-                    "current_value": "Any Location"
-                }, {"param": "Active on date", "current_value": "2017-03-01T00:00:00"}, {
-                    "param": "FT Keyword Search",
-                    "current_value": ""
-                }, {"param": "FT Facet", "current_value": "<ANY>"}, {
-                    "param": "FT Results Page Number",
-                    "current_value": 0
-                }, {"param": "FT Results Page Size", "current_value": 10}, {
-                    "param": "Search Field Trials",
-                    "current_value": false
-                }, {"param": "Fuzzy Search", "current_value": false}, {
-                    "param": "Get full data from search",
-                    "current_value": false
-                }, {"param": "Search Studies", "current_value": false}, {
-                    "param": "Get all Plots for Study",
-                    "current_value": false
-                }, {"param": "Get all Locations", "current_value": false}]
+                "parameters": [
+                    {
+                        "param": "FT Id",
+                        "current_value": fieldtrial_id
+                    },
+                    {
+                        "param": "FT Facet",
+                        "current_value": "Field Trial"
+                    },
+                    {
+                        "param": "FT Results Page Number",
+                        "current_value": 0
+                    },
+                    {
+                        "param": "FT Results Page Size",
+                        "current_value": 100
+                    }
+                ]
             }
         }]
     };
