1a080d5e85c8a4c0eaf199735903079ecfb40ea1
Xingdong Bian <bianxingdong@gmail.com>
2019-12-03 14:32:37 +0000

    now with control panel

--- dynamic/scripts/fieldtrial_map.js
+++ dynamic/scripts/fieldtrial_map.js
@@ -676,17 +676,14 @@ function get_GRU_by_accession(accession, plotId, id) {
 function format_gru_json(gru_json) {
     var htmlarray = [];
     if (gru_json != undefined && gru_json.length > 0) {
-        // for (i = 0; i < gru_json.length; i++) {
-        if (gru_json.length > 0) {
+        for (i = 0; i < gru_json.length; i++) {
+        // if (gru_json.length > 0) {
             if (gru_json[0]['idPlant'] != undefined) {
-
                 var idPlant = gru_json[i]['idPlant'];
-                // htmlarray.push(idPlant);
                 htmlarray.push('<a target="_blank" class="newstyle_link" href="https://seedstor.ac.uk/search-infoaccession.php?idPlant=' + idPlant + '">Plant ' + idPlant + '</a> ');
             }
         }
     }
-    // }
     return htmlarray.join('');
 }
 
